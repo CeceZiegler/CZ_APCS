64ca1ed677239c28b0da224e5e11d4087a465169
Cecelia Ziegler <cz1610341@035Labs-iMac.local>
2020-02-18 10:14:46 -0600

    new Upload

--- 044_AverageDiceRoll/AverageDiceRoll.java
+++ 044_AverageDiceRoll/AverageDiceRoll.java
@@ -1,8 +1,25 @@
 public class AverageDiceRoll
 {
+    //This method will get the average of all the dice rolls.
     public void getAverage()
     {
         OneThousandDiceRolls dr = new OneThousandDiceRolls();
-        for
+        //creating an array x from the OneThousandDiceRolls class.
+        int[] x = dr.getDiceRolls();
+        int total = 0;
+        //Using a for loop to get the total of all the dice rolls.
+        for(int i = 0; i < x.length; i++)
+        {
+            total += x[i];
+        }
+        int avg = total/1000;
+        System.out.println(avg);
+    }
+
+    public static void main(String[] args)
+    {
+        AverageDiceRoll ar = new AverageDiceRoll();
+        //Calling the get average method to print out the average dice rolls.
+        ar.getAverage();
     }
 }
